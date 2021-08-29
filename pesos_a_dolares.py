9a0c445eefa9c2a1c678cbbf71e7da621837a98d
Jorge Naranjo <jpna188@gmail.com>
2021-08-29 12:06:29 -0500

    Rounded 'dolares' to 3 decimal points

--- pesos_a_dolares.py
+++ pesos_a_dolares.py
@@ -1,5 +1,6 @@
 pesos = input('Ingrese un valor en pesos: ')
 pesos = float(pesos)
 dolares = pesos / 3829
+dolares = round(dolares, 3)
 dolares = str(dolares)
 print('Tienes $' + dolares + ' dolares.')
