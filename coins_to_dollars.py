1496460f826fc26081f6f9f1c5f5956d536b0b8a
Jorge Naranjo <jpna188@gmail.com>
2021-08-29 14:04:15 -0500

    Eliminated line 12

--- coins_to_dollars.py
+++ coins_to_dollars.py
@@ -10,7 +10,6 @@ Welcome to the money exchange
 print(menu)
 currency = int(input('Please select the number according to your currency: '))
 amount = float(input('Please select the amount you wish to exchange: '))
-dollars = 0
 if currency == 1:
 	dollars = amount * 0.79
 elif currency == 2:
@@ -19,4 +18,4 @@ elif currency == 3:
 	dollars = amount * 0.00050
 else:
 	print('You did not select a number between 1 and 3.')
-print(round(dollars, 3))
+print(str(round(dollars, 3)) + ' dollars')
